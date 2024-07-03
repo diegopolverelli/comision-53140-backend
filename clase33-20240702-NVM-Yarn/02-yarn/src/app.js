77023537754a803cccdab9db27fd3938e6d9f4e6
Diego Polverelli <diegopolverelli@hotmail.com>
2024-07-02 22:09:33 -0300

    clase 33 added

---
+++ clase33-20240702-NVM-Yarn/02-yarn/src/app.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import multer from "multer"
+const PORT=3000;
+
+const app=express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+app.get('/',(req,res)=>{
+    res.setHeader('Content-Type','text/plain');
+    res.status(200).send('OK');
+})
+
+const server=app.listen(PORT,()=>{
+    console.log(`Server escuchando en puerto ${PORT}`);
+});
